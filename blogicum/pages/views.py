965269ce07dcd894b1fade15682ad50dcbd37b7b
Denix <densavochka@mail.ru>
2025-02-20 10:03:19 +0700

    it works?

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -7,16 +7,20 @@ def about(request) -> HttpResponse:
     template: str = 'pages/about.html'
     return render(request, template)
 
+
 def rules(request) -> HttpResponse:
     """Правила проекта."""
     template: str = 'pages/rules.html'
     return render(request, template)
 
+
 def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
 
+
 def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
+
 
 def internal_server_error(request):
     return render(request, 'pages/500.html', status=500)
