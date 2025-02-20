d35523bb31c83861b1aac43a72471f1571b06aef
Denix <densavochka@mail.ru>
2025-02-20 11:01:35 +0700

    fix imports

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Count
-from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
 from django.http import HttpResponse
 from datetime import datetime
 
